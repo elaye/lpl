80ab08dda11905935326e7c491303dfc6e7296fc
elaye <genard.elie@orange.fr>
2017-06-24 09:38:43 +0100

    Remove unused utils

--- src/utils/date.js
+++
@@ -1,8 +0,0 @@
-function pad(n) {
-    return n < 10 ? `0${n}` : `${n}`;
-}
-
-export function today() {
-    const date = new Date();
-    return `${pad(date.getDate())}/${pad(date.getMonth())}/${date.getFullYear()}`;
-}
